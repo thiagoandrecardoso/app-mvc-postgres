cd0a11a1dc7f86c8555ee6e234650969d7126443
Thiago <thiagoandrecardoso@icloud.com>
2022-12-18 15:58:42 -0300

    Add new class Startup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
+using app_mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var startup = new Startup(builder.Configuration);
+
+startup.ConfigureServices(builder.Services);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+startup.Configure(app, app.Environment);
 
 app.Run();
