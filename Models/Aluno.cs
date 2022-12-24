685c0e440d7ba2efa70990e552fc79e69ac77736
Thiago <thiagoandrecardoso@icloud.com>
2022-12-24 14:26:28 -0300

    retirado metodos que criavam alunos locais

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -12,39 +12,5 @@ namespace app_mvc.Models
         public int Idade { get; set; }
         public char Genero { get; set; }
         public string? Curso { get; set; }
-        public List<Aluno>? _listaAluno;
-
-        public void CriarAluno()
-        {
-            try
-            {
-                _listaAluno = new List<Aluno>()
-                {
-                    new Aluno(){ Id = 1,Nome= "Paulo",Idade=29, Genero = 'M', Curso="Ciência da Computação"},
-                    new Aluno(){ Id = 2,Nome= "Alexandre",Idade=20, Genero = 'M', Curso="Economia"},
-                    new Aluno(){ Id = 3,Nome= "Carlos",Idade=25, Genero = 'M', Curso="Ciências Contábeis"},
-                    new Aluno(){ Id = 4,Nome= "Bianca",Idade=25, Genero = 'F', Curso="Ciência da Computação"},
-                    new Aluno(){ Id = 5,Nome= "Laura",Idade=35, Genero = 'F', Curso="Literatura"}
-                };
-            }
-            catch (System.Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-        }
-
-        public List<Aluno> BuscarAluno()
-        {
-            try
-            {
-                if(_listaAluno != null) 
-                    return _listaAluno;
-            }
-            catch (System.Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
-            return new List<Aluno>();
-        }
     }
 }
